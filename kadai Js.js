ef6bd595c69651d01739fb72c0cb7056dd92222c
栗原裕文 <kuriharahiromi@kuriharanomakku.local>
2021-12-16 17:01:24 +0900

    テスト

--- kadai Js.js
+++ kadai Js.js
@@ -2,3 +2,18 @@ const h1 = document.querySelector('h1')
 h1.animate({
   opacity: [0, 1]
 }, 1500)
+
+window.onload=function(){
+  var scroll = document.querySelectorAll('.up');
+   
+  var Animation = function() {
+    for(var i = 0; i < scroll.length; i++) {
+    var triggerMargin = 80;
+    if (window.innerHeight > scroll[i].getBoundingClientRect().top + triggerMargin) {
+    scroll[i].classList.add('show');
+    }
+    }
+    }
+    window.addEventListener('scroll', Animation);
+  
+}                               
